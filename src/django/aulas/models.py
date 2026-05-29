e4783468447ff6f7422d21c006e494927d9ffcdd
Axelalan05 <axeldavidalan05@gmail.com>
2026-05-29 19:48:19 +0000

    punto-9_creacion de modelos carrera, profesor, materia y aula

--- src/django/aulas/models.py
+++ src/django/aulas/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=128, null=False)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=128, null=False)
+    apellido = models.CharField(max_length=128, null=False)
+    mostrar = models.CharField(max_length=128, null=False)
+
+    def __str__(self):
+        return self.mostrar
+
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=128, null=False)
+    cant_alumnos = models.IntegerField(null=False, default=5)
+    id_carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    id_profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Aula(models.Model):
+    descripcion = models.CharField(max_length=128, null=False)
+    ubicacion = models.CharField(max_length=128, null=False)
+    cant_proyector = models.IntegerField(default=0)
+    aforo = models.IntegerField(default=0)
+    es_climatizada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.descripcion
